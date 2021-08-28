440fa526898627fcdd3671be93c60986a81e7d96
Dennis Kobert <dennis@kobert.dev>
2021-08-28 18:37:43 +0200

    ICE

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[no_mangle]
 pub extern "C" fn hello_rust() {
+    unsafe { libc::printf("hallo\n\n".as_ptr() as *const i8) };
     let a = vec![3, 4, 2, 1, 4, 2, 7];
     let b: Vec<_> = a.iter().map(|x| x + x).collect();
     for n in b {
