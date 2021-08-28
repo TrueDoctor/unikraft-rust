057e45a2d1fce3f72515f714ee42dc25a3e9bcb4
Dennis Kobert <dennis@kobert.dev>
2021-08-28 09:08:01 +0200

    Change memory allocator, try to use std print

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,13 @@
-//use core::panic::PanicInfo;
 #![feature(restricted_std, alloc_error_handler)]
 
-use linked_list_allocator::LockedHeap;
-
 #[global_allocator]
-static ALLOCATOR: LockedHeap = LockedHeap::empty();
-
-pub fn init_heap() {
-    let heap_start = 0x10000000;
-    let heap_end = 0x100000000;
-    let heap_size = heap_end - heap_start;
-    unsafe {
-        ALLOCATOR.lock().init(heap_start, heap_size);
-    }
-}
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[no_mangle]
-pub unsafe extern "C" fn hello_rust() {
-    printf("Hi!\n\0".as_ptr());
-    println!("HI");
+pub extern "C" fn hello_rust() {
+    let a = vec![3, 4, 2, 1, 4, 2, 7];
+    let b: Vec<_> = a.iter().map(|x| x + x).collect();
+    for n in b {
+        println!("HI {}", n);
+    }
 }
-
-extern "C" {
-    pub fn printf(format: *const u8, ...) -> i32;
-}
-
-/*
-#[panic_handler]
-fn panic(_panic: &PanicInfo<'_>) -> ! {
-    unsafe { printf("Rust panic\0\n".as_ptr()) };
-    loop {}
-}*/
