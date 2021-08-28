b2cbe7339be680c662f7eb8029f856eafaef1a7a
Dennis Kobert <dennis@kobert.dev>
2021-08-28 21:45:43 +0200

    Make loop example compile

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 UK_ROOT ?= $(PWD)/../../unikraft
 UK_LIBS ?= $(PWD)/../../libs
-LIBS := $(UK_LIBS)/lib-pthread-embedded:$(UK_LIBS)/lwip:$(UK_LIBS)/musl:$(UK_LIBS)/lib-newlib
+LIBS := $(UK_LIBS)/lib-pthread-embedded:$(UK_LIBS)/lwip:$(UK_LIBS)/lib-newlib
 
 all:
 	@$(MAKE) -C $(UK_ROOT) A=$(PWD) L=$(LIBS)
--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,26 @@
 #![feature(alloc_error_handler)]
+#![feature(default_alloc_error_handler)]
 #![no_std]
 
 use core::panic::PanicInfo;
+extern crate alloc;
+use alloc::vec::Vec;
 
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[no_mangle]
 pub extern "C" fn hello_rust() {
-    unsafe { libc::printf("hallo\n\n".as_ptr() as *const i8) };
-    /*let a = vec![3, 4, 2, 1, 4, 2, 7];
+    print("Hello rust\n\0");
+    let a = alloc::vec![3, 4, 2, 1, 4, 2, 7];
     let b: Vec<_> = a.iter().map(|x| x + x).collect();
     for n in b {
-        println!("HI {}", n);
-    }*/
+        print("HI from loop\n\0");
+    }
+}
+
+fn print(msg: &str) {
+    unsafe { libc::printf(msg.as_ptr() as *const i8) };
 }
 #[panic_handler]
 fn panic_rust(_: &PanicInfo) -> ! {
