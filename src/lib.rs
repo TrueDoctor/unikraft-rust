1caf8dce6c2f6b2dd34ad3006d273eefbb8c8931
Dennis Kobert <dennis@kobert.dev>
2021-08-28 20:51:17 +0200

    Setup without std

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
-#![feature(restricted_std, alloc_error_handler)]
+#![feature(alloc_error_handler)]
+#![no_std]
+
+use core::panic::PanicInfo;
 
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
@@ -6,9 +9,13 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 #[no_mangle]
 pub extern "C" fn hello_rust() {
     unsafe { libc::printf("hallo\n\n".as_ptr() as *const i8) };
-    let a = vec![3, 4, 2, 1, 4, 2, 7];
+    /*let a = vec![3, 4, 2, 1, 4, 2, 7];
     let b: Vec<_> = a.iter().map(|x| x + x).collect();
     for n in b {
         println!("HI {}", n);
-    }
+    }*/
+}
+#[panic_handler]
+fn panic_rust(_: &PanicInfo) -> ! {
+    loop {}
 }
